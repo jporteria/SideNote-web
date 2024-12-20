8da810a649fadc987d4db0a8e9129be89adfb991
jporteria <cjporteria@gmail.com>
2024-12-20 12:31:28 +0800

    convert code to run in web

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/SideNote-web/">
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',  // Set to the subpath where your app is hosted
+  base: '/SideNote-web/',  // Set to the subpath where your app is hosted
   build: {
     outDir: 'dist',
     assetsDir: 'assets', // Ensure the assets are placed correctly in the build output
