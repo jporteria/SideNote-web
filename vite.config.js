155f76cb631aba475634ee7d1c4042be63d48f4f
jporteria <cjporteria@gmail.com>
2024-12-20 12:00:35 +0800

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SideNote-web/",
+  base: '/',
+  build: {
+    outDir: 'dist', // Ensure this matches your build folder
+  },
   plugins: [react()],
-})
+});
